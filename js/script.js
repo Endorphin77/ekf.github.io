22fb5cd32218a27ae5cf98bfb9cda48903152f51
enorphine1998@yandex.ru <veselo1998@yandex.ru>
2021-06-10 22:54:22 +0300

    biosen11

--- js/script.js
+++ js/script.js
@@ -128,13 +128,26 @@ $(document).scroll(function(){
 
 
 
-
-
-
-
-
-
-
+$(document).ready(function(){
+  $('#button_contacts').click(function(){
+      var form_name   = $('#form_name').val();
+      var form_email   = $('#form_email').val();
+      var form_message = $('#form_message').val();
+      $.ajax({
+          url: "post.php", 
+          type: "post", 
+          dataType: "json", 
+          data: { 
+              "form_name":   form_name,
+              "form_email":   form_email,
+              "form_message": form_message
+          },
+          success: function(data){
+              $('.messages').html(data.result); 
+          }
+      });
+  });
+});
 
 
     window.addEventListener('DOMContentLoaded', () => {
@@ -155,4 +168,5 @@ $(document).scroll(function(){
       })
   })
 
+  
   
